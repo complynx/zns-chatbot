8e40f33160ec41249486179450206590d1418423
Daniel Drizhuk <d.drizhuk@gmail.com>
2023-05-19 18:40:57 -0300

    check user

--- zns-chatbot/server.py
+++ zns-chatbot/server.py
@@ -45,7 +45,29 @@ class PhotoHandler(tornado.web.StaticFileHandler):
             raise tornado.web.HTTPError(404)
         return absolute_path
 
+def check_hmac(data, secret_key):
+    import hashlib,hmac
+
+    # Extract the hash
+    tg_hash = data.get('hash')
+    # Make a copy of the data and remove the hash
+    data_without_hash = dict(data)
+    data_without_hash.pop('hash')
+    
+    # Sort the data by keys and serialize it to a check_string
+    check_string = "\n".join(f"{k}={data_without_hash[k]}" for k in sorted(data_without_hash.keys()))
+    
+    # Calculate a new hash using the secret_key and check_string
+    secret_key = hashlib.sha256(secret_key.encode()).digest()
+    new_hash = hmac.new(secret_key, check_string.encode(), digestmod=hashlib.sha256).hexdigest()
+    
+    # Compare the new hash with the received tg_hash
+    return new_hash == tg_hash
+
 class MenuHandler(tornado.web.RequestHandler):
+    def initialize(self, token):
+        self.token = token
+    
     async def post(self):
         import json
         # Get all the post form data
@@ -55,8 +77,13 @@ class MenuHandler(tornado.web.RequestHandler):
         for key in data:
             data[key] = [x.decode('utf-8') for x in data[key]]
 
+        tg_user = json.loads(data["tg_user"])
+        if not check_hmac(tg_user, self.token):
+            return self.write_error(401)
+        
+
         # If you want to send the data back as a response in pretty format
-        self.write(json.dumps(data, indent=4))
+        self.write(json.dumps(data, indent=4, ensure_ascii=False) + " " + json.dumps(tg_user, indent=4, ensure_ascii=False))
 
 
 
@@ -64,7 +91,7 @@ async def create_server(config: Config):
     tornado.platform.asyncio.AsyncIOMainLoop().install()
     app = tornado.web.Application([
         (r"/fit_frame", FitFrameHandler),
-        (r"/menu", MenuHandler),
+        (r"/menu", MenuHandler, {"token": config.telegram_token}),
         (r"/photos/(.*)", PhotoHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static/"}),
     ], template_path="templates/")
