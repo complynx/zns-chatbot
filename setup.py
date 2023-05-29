348953289696b8b941567efb0a57b7da898b9017
Daniel Drizhuk <d.drizhuk@gmail.com>
2023-05-29 15:38:54 -0300

    Add job queue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "python-telegram-bot==20.2",
+        "python-telegram-bot[job-queue]==20.2",
         "tornado==6.2",
         "pyyaml==6.0",
         "pillow==9.5",
