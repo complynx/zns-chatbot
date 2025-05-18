900956ca2401c7771249ce781f8750e8dbc47670
Daniel Drizhuk <d.drizhuk@gmail.com>
2025-05-18 11:18:03 +0200

    Change version to update text

--- zns-chatbot/telegram.py
+++ zns-chatbot/telegram.py
@@ -409,7 +409,7 @@ async def check_startup_actions(app):
     #             except Exception as e:
     #                 logger.error("Error in sender: %s", e, exc_info=1)
     #         await app.storage.set("sent_announcement", 1)
-    new_menu_version = 6
+    new_menu_version = 7
     if (
         "menu_version" not in app.storage
         or app.storage["menu_version"] != new_menu_version
